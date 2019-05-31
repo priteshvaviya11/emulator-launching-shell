c17030135d4ca61d83652aa98f3f8008c92b9009
priteshvaviya11 <45098507+priteshvaviya11@users.noreply.github.com>
2019-05-31 09:27:45 +0530

    Update emulator-launch.sh

--- emulator-launch.sh
+++ emulator-launch.sh
@@ -1,4 +1,4 @@
 cd /usr/lib/android-sdk/emulator
 export LD_LIBRARY_PATH=/u01/app/oracle/product/11.2.0/xe/lib:/usr/lib64:/usr/lib:/usr/lib/android-sdk/emulator/lib64
-export ANDROID_AVD_HOME=~/.android/avd
+export ANDROID_AVD_HOME=/root/.android/avd
 emulator -avd Pixel_API_26 -no-snapshot-save
